3841eaabea540cbf90b5698d2dd670ab59f86b6b
Cosme Oliveira <cosme25k@gmail.com>
2025-07-10 14:47:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
       {
         protocol: 'https',
         hostname: 'placehold.co',
